f148fe8b505e7f6034c13790027de4232106c395
ryansctt1994-sudo <ryan.sctt1994@gmail.com>
2026-06-13 01:14:10 -0500

    Export E3 upgrade primitives

--- src/lumen/__init__.py
+++ src/lumen/__init__.py
@@ -4,5 +4,16 @@ from .chronicle import Chronicle
 from .hardware_veto import HardwareVeto, LatchState
 from .receipt import Receipt
 from .replay import replay_ledger
+from .authority import VerificationResult, WeaverVerifier
+from .replay_cache import SQLiteReplayCache
 
-__all__ = ["Chronicle", "HardwareVeto", "LatchState", "Receipt", "replay_ledger"]
+__all__ = [
+    "Chronicle",
+    "HardwareVeto",
+    "LatchState",
+    "Receipt",
+    "replay_ledger",
+    "VerificationResult",
+    "WeaverVerifier",
+    "SQLiteReplayCache",
+]
