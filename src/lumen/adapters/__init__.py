21b66c7116b2b63189d68254f5ed0a1800785edf
ryansctt1994-sudo <ryan.sctt1994@gmail.com>
2026-06-13 01:04:13 -0500

    Add adapters package

---
+++ src/lumen/adapters/__init__.py
@@ -0,0 +1,5 @@
+"""Adapters for importing external/local-first concepts into Chronicle."""
+
+from .freelattice import FreeLatticeAdapter, FreeLatticeEntry
+
+__all__ = ["FreeLatticeAdapter", "FreeLatticeEntry"]
